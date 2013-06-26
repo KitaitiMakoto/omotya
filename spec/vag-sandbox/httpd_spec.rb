cb84436cbcc57b940cc9d0f40901d48512c25490
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-06-27 06:41:29 +0900

    Modify specs to fit to Nginx

--- spec/vag-sandbox/httpd_spec.rb
+++ spec/vag-sandbox/httpd_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe package('httpd') do
+describe package('nginx') do
   it { should be_installed }
 end
 
-describe service('httpd') do
+describe service('nginx') do
   it { should be_enabled   }
   it { should be_running   }
 end
@@ -13,7 +13,7 @@ describe port(80) do
   it { should be_listening }
 end
 
-describe file('/etc/httpd/conf/httpd.conf') do
+describe file('/etc/nginx/nginx.conf') do
   it { should be_file }
-  it { should contain "ServerName vag-sandbox" }
+  it { should contain "server_name omotya" }
 end
